15eeff9e229af33e09d2249072c48f7a73925994
mdallamaiqbal <bd999plus13@gmail.com>
2026-02-15 09:14:29 +0600

    addEventListener

--- js/event.js
+++ js/event.js
@@ -1,19 +1,31 @@
 // option 2 handle event
-    function makeYellow(){
-            document.body.style.backgroundColor='yellow'
-        }
-    function makeRed(){
-            document.body.style.backgroundColor='red'
-        }
+function makeYellow() {
+    document.body.style.backgroundColor = 'yellow'
+}
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red'
+}
 // option 3 get element by id 
- const btnMakeBlue=document.getElementById('btn-make-blue');
-      btnMakeBlue.onclick= function(){
-        document.body.style.backgroundColor='blue'
-      }
+const btnMakeBlue = document.getElementById('btn-make-blue');
+btnMakeBlue.onclick = function () {
+    document.body.style.backgroundColor = 'blue'
+}
 // option 3.1 different
- const btnMakePurple=document.getElementById('btn-make-purple');
-    //    btnMakePurple.onclick=makePurple(); function call kora jabe na sudu nam dili hobe
-        btnMakePurple.onclick=makePurple;
-       function makePurple(){
-        document.body.style.backgroundColor='purple'
-       }
+const btnMakePurple = document.getElementById('btn-make-purple');
+//    btnMakePurple.onclick=makePurple(); function call kora jabe na sudu nam dili hobe
+btnMakePurple.onclick = makePurple;
+
+function makePurple() {
+    document.body.style.backgroundColor = 'purple'
+}
+// option 4 add event listener
+//document.getElementId().addEventListener('event type',handler)
+document.getElementById('btn-make-green')
+.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green'
+});
+document.getElementById('btn-make-gold')
+.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'gold'
+});
