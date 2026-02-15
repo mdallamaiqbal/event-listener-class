932d02d6baa46d8f03400a1be2d9e70a4e4ab987
mdallamaiqbal <bd999plus13@gmail.com>
2026-02-15 08:59:59 +0600

    event listener class

---
+++ js/event.js
@@ -0,0 +1,19 @@
+// option 2 handle event
+    function makeYellow(){
+            document.body.style.backgroundColor='yellow'
+        }
+    function makeRed(){
+            document.body.style.backgroundColor='red'
+        }
+// option 3 get element by id 
+ const btnMakeBlue=document.getElementById('btn-make-blue');
+      btnMakeBlue.onclick= function(){
+        document.body.style.backgroundColor='blue'
+      }
+// option 3.1 different
+ const btnMakePurple=document.getElementById('btn-make-purple');
+    //    btnMakePurple.onclick=makePurple(); function call kora jabe na sudu nam dili hobe
+        btnMakePurple.onclick=makePurple;
+       function makePurple(){
+        document.body.style.backgroundColor='purple'
+       }
